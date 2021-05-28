e38b8f522113aa7c046091b2730ae0f49fdf4c7d
Богдан Куликовский <81013878+felix-luxury@users.noreply.github.com>
2021-05-28 16:58:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-btn.addEventlistener (click, Min(a,b),Option)
-
-function Min(a,b)
+function equal()
 {
-  document.getElementById("a").checked =a;
-  document.getElementById("b").checked =b;
-  a=Number.parseInt(a)
-  b=Number.parseInt(b)
-  if (a>b)
-  {
-      alert("Минимальное" =b)
-  }  
-  else
-  alert ("Максимальное")
+    d=document
+    var a = Number(d.form1.A.value);
+    var b = Number(d.form1.B.value);
+    var q;
+    if (a===b)
+    {
+        q="Числа равны";
+    }
+    else {q="Числа не равны";}
+    d.form1.q.value=q;
+}
 
-} 
+
+function Show()
+{
+    d=document
+    var admin, name;
+    name = "SHLYAPA";
+    admin = name;
+    alert(admin);
+}
+
+function Age()
+{
+    d=document
+    do {age = +prompt("Укажите ваш возраст");}
+    while 
+    (confirm("Вы уверены, что вам " +age)===false)
+    alert("Вам " +age);
+}
+
+function Com()
+{
+    d=document
+    let ok = confirm('Отправить данные на сервер')
+    if (ok) {
+        alert('Данные успешно отправлены на сервер');
+    } else {
+        alert('Отправка на сервер прервана!');
+    }
+}
